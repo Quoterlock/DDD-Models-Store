16cbcfb9d6eba55183dcfb2360865304e8906db6
Quoterlock <vladkrp2003@gmail.com>
2024-12-23 15:27:46 +0200

    configure auth and cookies

--- DddModelsStore/Program.cs
+++ DddModelsStore/Program.cs
@@ -1,6 +1,8 @@
 using DddModelsStore.DataAccess.Entities;
 using DddModelsStore.DataAccess;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("IdentityDbContextConnection") 
                        ?? throw new InvalidOperationException("Connection string 'IdentityDbContextConnection' not found.");
@@ -13,6 +15,33 @@ builder.Services.AddDefaultIdentity<AppUser>(options =>
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Config auth
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // password requirements
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    
+    // lock setting (prevent bruteforce)
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+    
+    // username settings
+    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyz1234567890_.-";
+});
+
+// Config cookies
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Identity/Account/Login";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+    options.LogoutPath = "/Identity/Account/Logout";
+});
 
 var app = builder.Build();
 
@@ -34,9 +63,11 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
 app.MapControllerRoute(
     name: "area_default",
     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
